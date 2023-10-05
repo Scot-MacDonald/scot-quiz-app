42a576287bd06b07c6fb0a4d215893bf8386943c
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-05 21:59:56 +0200

    more changes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const bodyElement = document.querySelector('[data-js="body"]');
-
 const toggleButton = document.querySelector('[data-js="toggle-button"]');
+const chk = document.getElementById("chk");
+
+chk.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+});
 
 toggleButton.addEventListener("click", () => {
   bodyElement.classList.toggle("dark");

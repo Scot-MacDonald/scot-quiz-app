f35c619800ff664d36ff621a6a130778ec42dec3
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-08 22:23:13 +0200

    used font awsome icons

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const bodyElement = document.querySelector('[data-js="body"]');
-const toggleButton = document.querySelector('[data-js="toggle-button"]');
-const chk = document.getElementById("chk");
+// const bodyElement = document.querySelector('[data-js="body"]');
+// const toggleButton = document.querySelector('[data-js="toggle-button"]');
+// const chk = document.getElementById("chk");
 
-chk.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
+const buttons = document.querySelectorAll('[data-js="button-selected"]');
+
+buttons.forEach(function (button) {
+  const icon = button.querySelector("i");
+
+  button.addEventListener("click", function () {
+    icon.classList.toggle("fa-regular");
+    icon.classList.toggle("fa-solid");
+  });
 });
 
-toggleButton.addEventListener("click", () => {
-  bodyElement.classList.toggle("dark");
-});
+// chk.addEventListener("change", () => {
+//   document.body.classList.toggle("dark");
+// });
+
+// toggleButton.addEventListener("click", () => {
+//   bodyElement.classList.toggle("dark");
+// });
+
+// button.addEventListener("click", function () {
+//   icon.classList.toggle("fa-regular");
+//   icon.classList.toggle("fa-solid");
+// });

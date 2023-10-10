66217d1c9655a9e77338d820badd16efdf3be156
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-10 14:05:47 +0200

    add answer toggle

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,10 +1,10 @@
 const buttons = document.querySelectorAll('[data-js="button-selected"]');
 
-buttons.forEach(function (button) {
-  const icon = button.querySelector("i");
+// buttons.forEach(function (button) {
+//   const icon = button.querySelector("i");
 
-  button.addEventListener("click", function () {
-    icon.classList.toggle("fa-regular");
-    icon.classList.toggle("fa-solid");
-  });
+buttons.addEventListener("click", (button) => {
+  icon.classList.toggle("fa-regular");
+  icon.classList.toggle("fa-solid");
 });
+// });
--- js/index.js
+++ js/index.js
@@ -1,27 +1,13 @@
-// const bodyElement = document.querySelector('[data-js="body"]');
-// const toggleButton = document.querySelector('[data-js="toggle-button"]');
-// const chk = document.getElementById("chk");
+const bodyElement = document.querySelector('[data-js="body"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+const answer = document.querySelector('[data-js="answer"]');
 
-// const buttons = document.querySelectorAll('[data-js="button-selected"]');
-
-// buttons.forEach(function (button) {
-//   const icon = button.querySelector("i");
-
-//   button.addEventListener("click", function () {
-//     icon.classList.toggle("fa-regular");
-//     icon.classList.toggle("fa-solid");
-//   });
-// });
-
-// chk.addEventListener("change", () => {
-//   document.body.classList.toggle("dark");
-// });
-
-// toggleButton.addEventListener("click", () => {
-//   bodyElement.classList.toggle("dark");
-// });
-
-// button.addEventListener("click", function () {
-//   icon.classList.toggle("fa-regular");
-//   icon.classList.toggle("fa-solid");
-// });
+toggleButton.addEventListener("click", function () {
+  if (answer.style.display === "block") {
+    answer.style.display = "none";
+    toggleButton.textContent = "Hide Answer";
+  } else {
+    answer.style.display = "block";
+    toggleButton.textContent = "Show Answer";
+  }
+});

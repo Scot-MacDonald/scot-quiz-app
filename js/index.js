ed75eebb5a887baff7d91053b50bb3779cd143d8
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-10 10:15:24 +0200

    add new html, js page and nav button

--- js/index.js
+++ js/index.js
@@ -2,16 +2,16 @@
 // const toggleButton = document.querySelector('[data-js="toggle-button"]');
 // const chk = document.getElementById("chk");
 
-const buttons = document.querySelectorAll('[data-js="button-selected"]');
+// const buttons = document.querySelectorAll('[data-js="button-selected"]');
 
-buttons.forEach(function (button) {
-  const icon = button.querySelector("i");
+// buttons.forEach(function (button) {
+//   const icon = button.querySelector("i");
 
-  button.addEventListener("click", function () {
-    icon.classList.toggle("fa-regular");
-    icon.classList.toggle("fa-solid");
-  });
-});
+//   button.addEventListener("click", function () {
+//     icon.classList.toggle("fa-regular");
+//     icon.classList.toggle("fa-solid");
+//   });
+// });
 
 // chk.addEventListener("change", () => {
 //   document.body.classList.toggle("dark");

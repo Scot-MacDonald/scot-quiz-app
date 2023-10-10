41390ec91858d59557638aef674b14577a119e48
Scot MacDonald <scotmacdonald@hotmail.com>
2023-10-10 12:16:34 +0200

    add javascript for form

---
+++ js/form.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('[data-js="form"]');
+const card = document.querySelector(".card");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  console.log(formData);
+
+  const newCard = Object.fromEntries(formData);
+
+  console.log(newCard);
+
+  const newAnswer = `<h2 class="card__question">${newCard.question}</div><p class="card__answer"> ${newCard.answer} </p><li class="card__tags__tag"> ${newCard.tag}</li>`;
+
+  const actualList = card.innerHTML;
+  card.innerHTML = newAnswer;
+});
